f1fbaab252249b0b1041e190def9ce2f24fa9f69
Rahul Das Adhikary <rahul.das.adhikary@gmail.com>
2019-03-28 04:22:44 +0530

    updating style
    updating style

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,364 +1,139 @@
 import React, { Component } from "react";
-import "./../css/Navigation.css";
 import Axios from "axios";
+import ReactDOM from "react-dom";
+import Products from "./Products";
 
+import "./../css/Navigation.css";
 /**
- * UserProfile is used to retrieve the customer details and update the information
+ * Registration class used for User Registration
  * @author Rahul Das Adhikary, rahul.das.adhikary@gmail.com, +91-9880978662
+ *
  */
-class UserProfile extends Component {
+class Registration extends Component {
   /**
-   * Construcor
-   * @param {*} props
+   * Saving customer information
    */
-  constructor(props) {
-    super(props);
-    this.state = {
-      custname: "",
-      custemail: "",
-      custpassword: "",
-      custday_phone: "",
-      custeve_phone: "",
-      custmob_phone: "",
-      custaddress_1: "",
-      custaddress_2: "",
-      custcity: "",
-      custcountry: "",
-      custpostal_code: "",
-      custregion: "",
-      custshipping_region_id: 0,
+  handleSubmit = event => {
+    event.preventDefault();
 
-      loading: false
-    };
-    this.getUserProfile = this.getUserProfile.bind(this);
-  }
+    let reg_name = document.getElementById("reg-name").value;
+    let reg_email = document.getElementById("reg-email").value;
+    let reg_password = document.getElementById("reg-password").value;
 
-  /**
-   * Generate the Customer details
-   */
-  getUserProfile() {
-    let jwtToken = sessionStorage.getItem("jwtToken");
-
-    //console.log(`Get Customer - ${jwtToken}`);
-    if (!jwtToken || jwtToken === "") {
-      alert("Please signin first to update the profile");
-      return;
-    }
-
-    Axios.get("https://backendapi.turing.com/customer", {
-      headers: {
-        "user-key": jwtToken
-      }
+    //Saving customer information
+    Axios.post(`https://backendapi.turing.com/customers`, {
+      name: reg_name,
+      email: reg_email,
+      password: reg_password
     })
-      .then(json => {
-        //console.log(json.data);
-        this.setState({
-          custname: !json.data.name ? "" : json.data.name,
-          custemail: !json.data.email ? "" : json.data.email,
-          custpassword: "", //!json.data.password ? "" : json.data.password,
-          custday_phone: !json.data.day_phone ? "" : json.data.day_phone,
-          custeve_phone: !json.data.eve_phone ? "" : json.data.eve_phone,
-          custmob_phone: !json.data.mob_phone ? "" : json.data.mob_phone,
-          custaddress_1: !json.data.address_1 ? "" : json.data.address_1,
-          custaddress_2: !json.data.address_2 ? "" : json.data.address_2,
-          custcity: !json.data.city ? "" : json.data.city,
-          custcountry: !json.data.country ? "" : json.data.country,
-          custpostal_code: !json.data.postal_code ? "" : json.data.postal_code,
-          custregion: !json.data.region ? "" : json.data.region,
-          custshipping_region_id: !json.data.shipping_region_id
-            ? ""
-            : json.data.shipping_region_id
-        });
+      .then(res => {
+        //console.log(res.data);
+        sessionStorage.setItem("jwtToken", res.data.accessToken);
+        sessionStorage.setItem("loggedInUser", res.data.customer.name);
+
+        document.getElementById("signinplaceholder").innerText = "Sign Out";
+        document.getElementById("usernamemenu").innerText =
+          res.data.customer.name;
+
+        var div = document.createElement("div");
+        ReactDOM.render(<Products />, div);
+        document.getElementById("productPanel").childNodes[0].remove();
+        document.getElementById("productPanel").appendChild(div);
       })
       .catch(e => {
-        console.error("Failed to retrieve Customer data .", e);
         let errMesg = e.response.data.error.message;
-        console.error("Failed to retrieve customer", errMesg);
+        console.error("Failed to register the user:", errMesg);
+        alert(`Failed to register the user. ${errMesg}`);
+
+        return;
       });
-  }
-  componentDidMount() {
-    this.getUserProfile();
-  }
-
-  /**
-   * This will update the customer information
-   *
-   */
-
-  updateCustomerData = event => {
-    event.preventDefault();
-
-    let jwtToken = sessionStorage.getItem("jwtToken");
-    //console.log(`Put Customer - ${jwtToken}`);
-
-    Axios.put(
-      "https://backendapi.turing.com/customer",
-
-      {
-        name: this.state.custname,
-        email: this.state.custemail,
-        password: this.state.custpassword,
-        day_phone: this.state.custday_phone,
-        eve_phone: this.state.custeve_phone,
-        mob_phone: this.state.custmob_phone
-      },
-      {
-        headers: {
-          "user-key": jwtToken
-        }
-      }
-    )
-      .then(json => {
-        //console.log(`Successfull ${json.data}`);
-      })
-      .catch(e => {
-        //console.error("Failed to save Customer data .", e);
-        let errMesg = e.response.data.error.message;
-        console.error("Failed to update", errMesg);
-      });
-  };
-
-  /**
-   * Used for form data binding
-   */
-  handleInputChange = event => {
-    event.preventDefault();
-
-    this.setState({
-      [event.target.name]: event.target.value
-    });
   };
 
   /**
    * Default component renderer
    */
   render() {
-    const { custname } = this.state;
-    const { custemail } = this.state;
-    const { custpassword } = this.state;
-    const { custday_phone } = this.state;
-    const { custeve_phone } = this.state;
-    const { custmob_phone } = this.state;
-    const { custaddress_1 } = this.state;
-    const { custaddress_2 } = this.state;
-    const { custcity } = this.state;
-    const { custcountry } = this.state;
-    const { custpostal_code } = this.state;
-    const { custregion } = this.state;
-    const { custshipping_region_id } = this.state;
-
     return (
       <div align="center">
-        <h3 className="textcol_orange">Customer Profile </h3>
+        <h3 className="textcol_orange">User Registration </h3>
         <small>
-          <table>
-            <tbody>
-              <tr>
-                <td className="form_textlabel">
-                  Full name
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custname"
-                    value={custname}
-                    placeholder="Full name"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Email
-                  <i className="required" />:
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="custemail"
-                    value={custemail}
-                    placeholder="Email address"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Password:</td>
-                <td>
-                  <input
-                    type="password"
-                    name="custpassword"
-                    value={custpassword}
-                    placeholder="Password"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Day Phone:</td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custday_phone"
-                    value={custday_phone}
-                    placeholder="Day phone"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Evening Phone:</td>
-                <td>
-                  <input
-                    type="text"
-                    name="custeve_phone"
-                    value={custeve_phone}
-                    placeholder="Evening phone"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Mobile Phone:</td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custmob_phone"
-                    value={custmob_phone}
-                    placeholder="Day phone"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Address 1<i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custaddress_1"
-                    value={custaddress_1}
-                    placeholder="Address 1"
-                    onChange={this.handleInputChange}
-                    className="form_text_long"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Address 2:</td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custaddress_2"
-                    value={custaddress_2}
-                    placeholder="Address 2"
-                    onChange={this.handleInputChange}
-                    className="form_text_long"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  City
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custcity"
-                    value={custcity}
-                    placeholder="City"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Region
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custregion"
-                    value={custregion}
-                    placeholder="Region"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Postal Code
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custpostal_code"
-                    value={custpostal_code}
-                    placeholder="Postal Code:"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Country
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custcountry"
-                    value={custcountry}
-                    placeholder="Country:"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-
-              <tr>
-                <td className="form_textlabel">
-                  Shipping Region
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="number"
-                    name="custshipping_region_id"
-                    value={custshipping_region_id}
-                    placeholder="Shipping Region"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <b>
+            <table>
+              <tbody>
+                <tr>
+                  <td className="form_textlabel">
+                    Full Name:
+                    <i className="required" />
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      id="reg-name"
+                      name="reg-name"
+                      placeholder="Full name"
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">
+                    New Password:
+                    <i className="required" />
+                  </td>
+                  <td>
+                    <input
+                      type="password"
+                      id="reg-password"
+                      name="reg-password"
+                      placeholder="New Password"
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">
+                    Confirm Password:
+                    <i className="required" />
+                  </td>
+                  <td>
+                    <input
+                      type="password"
+                      id="reg-confirm_password"
+                      name="reg-confirm_password"
+                      placeholder="Confirm Password"
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">
+                    Email Address:
+                    <i className="required" />
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      id="reg-email"
+                      name="reg-email"
+                      placeholder="Email address"
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </b>
         </small>
-        <p />
-
         <div align="center">
-          <button className="button_login" onClick={this.updateCustomerData}>
-            <b>Update</b>
-          </button>
+          <p>
+            <button className="button_login" onClick={this.handleSubmit}>
+              <b>Sign Up</b>
+            </button>
+          </p>
         </div>
       </div>
     );
   }
 }
 
-export default UserProfile;
+export default Registration;

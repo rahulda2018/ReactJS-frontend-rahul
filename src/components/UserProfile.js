30cb53603bb01758020cf41538788b1bf4dcf2cb
Rahul Das Adhikary <rahul.das.adhikary@gmail.com>
2019-03-28 17:23:13 +0530

    updating PUT call for customer and address
    updating PUT call for customer and address

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -93,7 +93,6 @@ class UserProfile extends Component {
 
     Axios.put(
       "https://backendapi.turing.com/customer",
-
       {
         name: this.state.custname,
         email: this.state.custemail,
@@ -109,12 +108,42 @@ class UserProfile extends Component {
       }
     )
       .then(json => {
-        //console.log(`Successfull ${json.data}`);
+        console.log(`Successfull ${json.data}`);
+        sessionStorage.setItem("loggedInUser", this.state.custname);
+        document.getElementById("usernamemenu").innerText = this.state.custname;
+
+        Axios.put(
+          "https://backendapi.turing.com/customers/address",
+          {
+            address_1: this.state.custaddress_1,
+            address_2: this.state.custaddress_2,
+            city: this.state.custcity,
+            country: this.state.custcountry,
+            postal_code: this.state.custpostal_code,
+            region: this.state.custregion,
+            shipping_region_id: this.state.custshipping_region_id
+          },
+          {
+            headers: {
+              "user-key": jwtToken
+            }
+          }
+        )
+          .then(json => {
+            alert("Successfully updated Customer profile and address");
+          })
+          .catch(e => {
+            //console.error("Failed to save Customer data .", e);
+            let errMesg = e.response.data.error.message;
+            console.error("Failed to update customer address", errMesg);
+            alert(`Failed to update customer address: ${errMesg}`);
+          });
       })
       .catch(e => {
         //console.error("Failed to save Customer data .", e);
         let errMesg = e.response.data.error.message;
-        console.error("Failed to update", errMesg);
+        console.error("Failed to update customer profile", errMesg);
+        alert(`Failed to update customer profile: ${errMesg}`);
       });
   };
 
@@ -127,6 +156,12 @@ class UserProfile extends Component {
     this.setState({
       [event.target.name]: event.target.value
     });
+  };
+
+  handleAddressChange = event => {
+    event.preventDefault();
+    console.log(`Value: ${event.target.value}`);
+    document.getElementById("address_table");
   };
 
   /**
@@ -148,211 +183,237 @@ class UserProfile extends Component {
     const { custshipping_region_id } = this.state;
 
     return (
-      <div align="center">
-        <h3 className="textcol_orange">Customer Profile </h3>
+      <div align="menu__list">
         <small>
-          <table>
-            <tbody>
-              <tr>
-                <td className="form_textlabel">
-                  Full name
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custname"
-                    value={custname}
-                    placeholder="Full name"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Email
-                  <i className="required" />:
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    name="custemail"
-                    value={custemail}
-                    placeholder="Email address"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Password:</td>
-                <td>
-                  <input
-                    type="password"
-                    name="custpassword"
-                    value={custpassword}
-                    placeholder="Password"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Day Phone:</td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custday_phone"
-                    value={custday_phone}
-                    placeholder="Day phone"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Evening Phone:</td>
-                <td>
-                  <input
-                    type="text"
-                    name="custeve_phone"
-                    value={custeve_phone}
-                    placeholder="Evening phone"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Mobile Phone:</td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custmob_phone"
-                    value={custmob_phone}
-                    placeholder="Day phone"
-                    onChange={this.handleInputChange}
-                    className="form_text"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Address 1<i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custaddress_1"
-                    value={custaddress_1}
-                    placeholder="Address 1"
-                    onChange={this.handleInputChange}
-                    className="form_text_long"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">Address 2:</td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custaddress_2"
-                    value={custaddress_2}
-                    placeholder="Address 2"
-                    onChange={this.handleInputChange}
-                    className="form_text_long"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  City
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custcity"
-                    value={custcity}
-                    placeholder="City"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Region
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custregion"
-                    value={custregion}
-                    placeholder="Region"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Postal Code
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custpostal_code"
-                    value={custpostal_code}
-                    placeholder="Postal Code:"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-              <tr>
-                <td className="form_textlabel">
-                  Country
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="text"
-                    name="custcountry"
-                    value={custcountry}
-                    placeholder="Country:"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
+          <p>&nbsp;</p>
+          <div className="form_div">
+            <h3 className="textcol_orange">
+              <small>Customer Profile </small>
+            </h3>
+            <table align="center">
+              <tbody>
+                <tr>
+                  <td className="form_textlabel">
+                    Full name
+                    <i className="required" />:
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custname"
+                      value={custname}
+                      placeholder="Full name"
+                      onChange={this.handleInputChange}
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">
+                    Email
+                    <i className="required" />:
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      name="custemail"
+                      value={custemail}
+                      placeholder="Email address"
+                      onChange={this.handleInputChange}
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">Password:</td>
+                  <td>
+                    <input
+                      type="password"
+                      name="custpassword"
+                      value={custpassword}
+                      placeholder="Password"
+                      onChange={this.handleInputChange}
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">Day Phone:</td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custday_phone"
+                      value={custday_phone}
+                      placeholder="Day phone"
+                      onChange={this.handleInputChange}
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">Evening Phone:</td>
+                  <td>
+                    <input
+                      type="text"
+                      name="custeve_phone"
+                      value={custeve_phone}
+                      placeholder="Evening phone"
+                      onChange={this.handleInputChange}
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel">Mobile Phone:</td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custmob_phone"
+                      value={custmob_phone}
+                      placeholder="Day phone"
+                      onChange={this.handleInputChange}
+                      className="form_text"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <p>&nbsp;</p>
 
-              <tr>
-                <td className="form_textlabel">
-                  Shipping Region
-                  <i className="required" />:
-                </td>
-                <td alight="left">
-                  <input
-                    type="number"
-                    name="custshipping_region_id"
-                    value={custshipping_region_id}
-                    placeholder="Shipping Region"
-                    onChange={this.handleInputChange}
-                    className="form_text_small"
-                  />
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="form_div">
+            <h3 className="textcol_orange">
+              <small>Customer Address </small>
+            </h3>
+            <table align="center" id="address_table">
+              <tbody>
+                <tr>
+                  <td className="form_textlabel_long">
+                    Address 1<i className="required" />:
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custaddress_1"
+                      value={custaddress_1}
+                      placeholder="Address 1"
+                      onChange={this.handleInputChange}
+                      className="form_text_long"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel_long">Address 2:</td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custaddress_2"
+                      value={custaddress_2}
+                      placeholder="Address 2"
+                      onChange={this.handleInputChange}
+                      className="form_text_long"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel_long">
+                    City
+                    <i className="required" />:
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custcity"
+                      value={custcity}
+                      placeholder="City"
+                      onChange={this.handleInputChange}
+                      className="form_text_small"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel_long">
+                    Region
+                    <i className="required" />:
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custregion"
+                      value={custregion}
+                      placeholder="Region"
+                      onChange={this.handleInputChange}
+                      className="form_text_small"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel_long">
+                    Postal Code
+                    <i className="required" />:
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custpostal_code"
+                      value={custpostal_code}
+                      placeholder="Postal Code:"
+                      onChange={this.handleInputChange}
+                      className="form_text_small"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="form_textlabel_long">
+                    Country
+                    <i className="required" />:
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="text"
+                      name="custcountry"
+                      value={custcountry}
+                      placeholder="Country:"
+                      onChange={this.handleInputChange}
+                      className="form_text_small"
+                    />
+                  </td>
+                </tr>
+
+                <tr>
+                  <td className="form_textlabel_long">
+                    Shipping Region
+                    <i className="required" />:
+                  </td>
+                  <td alight="left">
+                    <input
+                      type="number"
+                      name="custshipping_region_id"
+                      value={custshipping_region_id}
+                      placeholder="Shipping Region"
+                      onChange={this.handleInputChange}
+                      className="form_text_small"
+                    />
+                  </td>
+                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </small>
         <p />
 
         <div align="center">
-          <button className="button_login" onClick={this.updateCustomerData}>
+          <button
+            id="save_button"
+            className="button_login"
+            onClick={this.updateCustomerData}
+          >
             <b>Update</b>
           </button>
         </div>
